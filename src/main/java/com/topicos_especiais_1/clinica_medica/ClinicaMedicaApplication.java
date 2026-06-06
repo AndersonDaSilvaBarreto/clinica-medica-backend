85d8b84731ce0380bf2649f6086d800bd83c0744
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-06-06 12:01:05 -0300

    Initial commit

---
+++ src/main/java/com/topicos_especiais_1/clinica_medica/ClinicaMedicaApplication.java
@@ -0,0 +1,12 @@
+package com.topicos_especiais_1.clinica_medica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/topicos_especiais_1/clinica_medica/ClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicos_especiais_1.clinica_medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
